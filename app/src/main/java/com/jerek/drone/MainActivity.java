e085560757a9e9e4b13b3e70c591501725103312
jerekshoe <jerek@passfail.com>
2016-05-01 14:05:55 -0500

    added settings menu

--- app/src/main/java/com/jerek/drone/MainActivity.java
+++ app/src/main/java/com/jerek/drone/MainActivity.java
@@ -1,13 +1,131 @@
 package com.jerek.drone;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStreamButton;
+    private TextView mStreamText;
+    private int mIP1 = 192;
+    private int mIP2= 168;
+    private int mIP3 = 1;
+    private int mIP4 = 4;
+    private int mIPPort = 8080;
+    private String mIPCommand = "?action=stream";
+    private String mURL;
+    private static final int REQUEST_SETTINGS = 0;
+
+    private static final String TAG = "MainActivity";
+
+    static {
+        if(!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV not loaded");
+        }
+        else {
+            Log.d(TAG, "OpenCV loaded");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences preferences = getSharedPreferences("SAVED_VALUES", MODE_PRIVATE);
+        mIP1 = preferences.getInt("ip_ad1", mIP1);
+        mIP2 = preferences.getInt("ip_ad2", mIP2);
+        mIP3 = preferences.getInt("ip_ad3", mIP3);
+        mIP4 = preferences.getInt("ip_ad4", mIP4);
+        mIPPort = preferences.getInt("ip_port", mIPPort);
+        mIPCommand = preferences.getString("ip_command", mIPCommand);
+
+        StringBuilder sb = new StringBuilder();
+        String sb_http = "http://";
+        String sb_dot = ".";
+        String sb_colon = ":";
+        String sb_slash = "/";
+
+        sb.append(sb_http);
+        sb.append(mIP1);
+        sb.append(sb_dot);
+        sb.append(mIP2);
+        sb.append(sb_dot);
+        sb.append(mIP3);
+        sb.append(sb_dot);
+        sb.append(mIP4);
+        sb.append(sb_colon);
+        sb.append(mIPPort);
+        sb.append(sb_slash);
+        sb.append(mIPCommand);
+        mURL = new String(sb);
+
         setContentView(R.layout.activity_main);
+        mStreamButton = (Button) findViewById(R.id.stream_button);
+        mStreamText = (TextView) findViewById(R.id.stream_string);
+        mStreamText.setText(mURL);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            Intent settings_intent = new Intent(this, SettingsActivity.class);
+            settings_intent.putExtra("ip_address_1", mIP1);
+            settings_intent.putExtra("ip_address_2", mIP2);
+            settings_intent.putExtra("ip_address_3", mIP3);
+            settings_intent.putExtra("ip_address_4", mIP4);
+            settings_intent.putExtra("ip_port", mIPPort);
+            settings_intent.putExtra("ip_command", mIPCommand);
+            startActivityForResult(settings_intent, REQUEST_SETTINGS);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_SETTINGS:
+                if (resultCode == Activity.RESULT_OK) {
+                    mIP1 = data.getIntExtra("ip_address_1", mIP1);
+                    mIP2 = data.getIntExtra("ip_address_2", mIP2);
+                    mIP3 = data.getIntExtra("ip_address_3", mIP3);
+                    mIP4 = data.getIntExtra("ip_address_4", mIP4);
+                    mIPPort = data.getIntExtra("ip_port", mIPPort);
+                    mIPCommand = data.getStringExtra("ip_command");
+
+                    SharedPreferences preferences = getSharedPreferences("SAVED_VALUES", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+
+                    editor.putInt("ip_ad1", mIP1);
+                    editor.putInt("ip_ad2", mIP2);
+                    editor.putInt("ip_ad3", mIP3);
+                    editor.putInt("ip_ad4", mIP4);
+                    editor.putInt("ip_port", mIPPort);
+                    editor.putString("ip_command", mIPCommand);
+
+                    editor.apply();
+                }
+            break;
+        }
     }
 }
+
---
+++ app/src/main/java/com/jerek/drone/SettingsActivity.java
@@ -0,0 +1,108 @@
+package com.jerek.drone;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private EditText ip_input_1;
+    private EditText ip_input_2;
+    private EditText ip_input_3;
+    private EditText ip_input_4;
+    private EditText port_input;
+    private EditText command_input;
+
+    private int ip_address_1 = 192;
+    private int ip_address_2 = 168;
+    private int ip_address_3 = 1;
+    private int ip_address_4 = 4;
+    private int ip_port = 8080;
+    private String ip_command = "?action=stream";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_HOME_AS_UP, ActionBar.DISPLAY_HOME_AS_UP);
+        }
+
+        Bundle extras = getIntent().getExtras();
+
+        ip_input_1 = (EditText) findViewById(R.id.settings_ip_1);
+        ip_input_2 = (EditText) findViewById(R.id.settings_ip_2);
+        ip_input_3 = (EditText) findViewById(R.id.settings_ip_3);
+        ip_input_4 = (EditText) findViewById(R.id.settings_ip_4);
+        port_input = (EditText) findViewById(R.id.settings_port);
+        command_input = (EditText) findViewById(R.id.settings_command);
+
+        if(extras != null) {
+            ip_address_1 = extras.getInt("ip_address_1", ip_address_1);
+            ip_address_2 = extras.getInt("ip_address_2", ip_address_2);
+            ip_address_3 = extras.getInt("ip_address_3", ip_address_3);
+            ip_address_4 = extras.getInt("ip_address_4", ip_address_4);
+            ip_port = extras.getInt("ip_port", ip_port);
+            ip_command = extras.getString("ip_command");
+
+            ip_input_1.setText(String.valueOf(ip_address_1));
+            ip_input_2.setText(String.valueOf(ip_address_2));
+            ip_input_3.setText(String.valueOf(ip_address_3));
+            ip_input_4.setText(String.valueOf(ip_address_4));
+            port_input.setText(String.valueOf(ip_port));
+            command_input.setText(String.valueOf(ip_command));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: {
+                String s;
+
+                s = ip_input_1.getText().toString();
+                if (!"".equals(s)) {
+                    ip_address_1 = Integer.parseInt(s);
+                }
+                s = ip_input_2.getText().toString();
+                if (!"".equals(s)) {
+                    ip_address_2 = Integer.parseInt(s);
+                }
+                s = ip_input_3.getText().toString();
+                if (!"".equals(s)) {
+                    ip_address_3 = Integer.parseInt(s);
+                }
+                s = ip_input_4.getText().toString();
+                if (!"".equals(s)) {
+                    ip_address_4 = Integer.parseInt(s);
+                }
+
+                s = port_input.getText().toString();
+                if (!"".equals(s)) {
+                    ip_port = Integer.parseInt(s);
+                }
+
+                s = command_input.getText().toString();
+                ip_command = s;
+
+                Intent intent = new Intent();
+                intent.putExtra("ip_address_1", ip_address_1);
+                intent.putExtra("ip_address_2", ip_address_2);
+                intent.putExtra("ip_address_3", ip_address_3);
+                intent.putExtra("ip_address_4", ip_address_4);
+                intent.putExtra("ip_port", ip_port);
+                intent.putExtra("ip_command", ip_command);
+
+                setResult(RESULT_OK, intent);
+                finish();
+                return true;
+            }
+        }
+        return false;
+    }
+ }
